656083fd709f79b3547ca16cd2140567a20fd09d
Prabh-2004 <prabhjsm@gmail.com>
2026-04-27 22:39:58 +0530

    Model evaluation pipeline

---
+++ src/pipelines/model_evaluation_pipeline.py
@@ -0,0 +1,14 @@
+from src.logging import logger
+from src.config.configuration import ConfigurationManager
+from src.components.model_evalution import ModelEvaluation
+
+class ModelEvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def initiate_model_evaluation(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation.evaluate_model()
+        
